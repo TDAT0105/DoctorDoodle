6eb27257b5fe70235aa89a6bbf266ffc7e52a52b
TDAT0105 <ngotrungdat123@gmail.com>
2024-08-15 00:44:15 +0700

    Fixed Tailwind CSS doesnt work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import Navbar from "@/components/NavBar";
-
+import bg from '../assets/images/japan-cupyb43e8vl4w2n7.jpg';
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div
+        className="bg-contain bg-no-repeat bg-center"
+        style={{
+          backgroundImage: `url(${bg.src})`,
+          height: "500px", // Chiều cao bằng toàn bộ viewport height
+          width: "100%", // Chiếm toàn bộ chiều rộng của màn hình
+          backgroundSize: "cover", // Đảm bảo hình nền phủ toàn bộ không gian
+          backgroundPosition: "center center" // Canh giữa hình nền
+        }}
+      >
+      </div>
+      <div className="pt-16 pl-16">
+        <h1 className="text-2xl">Top Rated Doctors Near You</h1>
+      </div>
+
+
+    </>
   );
 }

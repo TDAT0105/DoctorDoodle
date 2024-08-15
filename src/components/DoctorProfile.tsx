fdd6358149f87b3befdb78001ca943c04f44d2e4
TDAT0105 <ngotrungdat123@gmail.com>
2024-08-16 00:23:57 +0700

    fixed padding and added hover for button vs text button

--- src/components/DoctorProfile.tsx
+++ src/components/DoctorProfile.tsx
@@ -13,17 +13,21 @@ const DoctorList: React.FC = () => {
             </h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-6 gap-8">
                 {visibleDoctors.map((doctor: Doctor) => (
-                    <div key={doctor.id} className="bg-white rounded-lg overflow-hidden border-2 border-gray-200  pb-1 pl-1 pr-1">
+                    <div key={doctor.id} className="bg-white rounded-lg overflow-hidden border-2 border-gray-200  pb-2 pl-2 pr-2">
                         <div className="relative w-full h-48">
                             <img src={doctor.imageUrl} alt={doctor.name} className="absolute inset-0 w-full h-full object-contain" />
                         </div>
                         <div className="">
                             <h3 className="font-semibold overflow-hidden">{doctor.name}</h3>
+
                             <h4 className="text-gray-600 overflow-hidden">{doctor.address}</h4>
+
                             <div className="flex justify-center pt-8">
-                                <button className='w-full rounded-md border-2 border-dark-blue py-2 px-4 flex items-center justify-center text-dark-blue'>
+                                <button className='w-full rounded-md border-2 border-dark-blue py-2 px-4 flex items-center justify-center text-dark-blue 
+                   hover:bg-dark-blue hover:text-white transition duration-300'>
                                     Consult Now
                                 </button>
+
                             </div>
 
                         </div>
@@ -31,7 +35,7 @@ const DoctorList: React.FC = () => {
                 ))}
             </div>
             <div className="pt-8">
-                <a href='#' className='text-primary-grey underline underline-offset-2'>View all</a>
+                <a href='#' className='text-primary-grey underline underline-offset-2 hover:text-dark-blue transition duration-300'>View all</a>
             </div>
         </div>
     );
